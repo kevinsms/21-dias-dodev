33dc8c89cb80919d7bf12856ea53c52536479998
Kevin Silva <kevin.mss@hotmail.com>
2024-03-26 21:47:40 -0300

    Finalizando os exercícios do dia 9

--- dia-9/index.js
+++ dia-9/index.js
@@ -1,9 +1,11 @@
+let condicao;
+do{
 //Escreva um algoritmo onde seu usuário deve informar nome, idade, peso, altura e profissão, a partir disso você deve exibir um texto com todas essas informações. 
 //Ex: Usuário inseriu nome: Giovanni, idade: 21, peso: 74kg, profissão: Programador, altura: 1.83, Você exibe: Olá Giovanni, você tem 21 anos, é programador, 
 //tem 1.83M de altura e pesa 74kg.
 //Verifique se o usuário é maior ou menor de idade, se ele for maior exiba na tela “Está liberado para tomar umas geladas”, se ele for menor exiba “Sem gelada para você”.
 //Utilize a idade informada pelo usuário, e mostre sua idade em meses, semanas e dias. Considere ano com 365 dias, mês com 30 dias e semana com 7 dias. 
-
+console.clear();
 let nome = prompt('Digite seu nome:')
 let idade = Number(prompt('Digite sua idade:'))
 let peso = Number(prompt('Digite seu peso:'))
@@ -47,3 +49,15 @@ if (imc < 18.5) {
 } else {
     console.log('Obesidade')
 }
+
+let anoAtual = 2024
+let anoNasc = anoAtual - idade
+
+console.log(`Você nasceu no ano de ${anoNasc}`)
+
+for(let i = 0; anoNasc <= anoAtual; i++){
+    console.log(`${anoNasc} - ${i} anos de idade`)
+    anoNasc++
+}
+condicao = Number(prompt('Deseja continuar ? 1-(SIM) 2-(NÃO)'))
+}while(condicao === 1);
