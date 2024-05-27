26aba79e4e0a650e738d8999a48d1227d3c7995f
Kevin <kevin.santos@printi.com.br>
2024-05-27 11:43:17 -0300

    Adicionando exercicio 2 do dia 6

---
+++ dia-6/index2.js
@@ -0,0 +1,10 @@
+//Faça um algoritmo utilizando a estrutura de repetição FOR que receba 1 número e exiba na tela qual o resultado da sua tabuada e dos próximos 2 números.
+
+let tabuada = Number(prompt('Escolha a tabuada que deseja'))
+
+for(let contador = 0; contador <=10; contador++){
+    console.log(tabuada + ' X ' + contador + " = " + tabuada*contador)
+}for(let contador = 0; contador <=10; contador++){
+    
+}
+
